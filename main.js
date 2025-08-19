469d10ae5adbd7270be645b8c5804ea0a54b5585
Working On It <15123120+working-on-it@user.noreply.gitee.com>
2025-08-19 16:14:35 +0800

    feat: init electron framework

---
+++ main.js
@@ -0,0 +1,90 @@
+// Copyright 2025 The Casibase Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+"use strict"
+const { app, BrowserWindow, ipcMain } = require("electron")
+const path = require("path")
+const url = require("url")
+
+let mainWindow
+
+let dev = false
+
+if (process.env.NODE_ENV !== undefined && process.env.NODE_ENV === "development") {
+  dev = true
+}
+
+ipcMain.on("window-minimize", () => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (win) win.minimize();
+});
+
+ipcMain.on("window-close", () => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (win) win.close();
+});
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  let indexPath
+
+  if (dev && process.argv.indexOf("--noDevServer") === -1) {
+    indexPath = url.format({
+      protocol: "http:",
+      host: "localhost:8080",
+      pathname: "index.html",
+      slashes: true
+    })
+  } else {
+    indexPath = url.format({
+      protocol: "file:",
+      pathname: path.join(__dirname, "dist", "index.html"),
+      slashes: true
+    })
+  }
+
+  mainWindow.loadURL(indexPath)
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show()
+  })
+
+  mainWindow.on("closed", function () {
+    mainWindow = null
+  })
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit()
+  }
+})
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
